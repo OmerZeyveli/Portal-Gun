eb4517fc66239db418c383f5a139aa2a9d8466cc
Riive <omer.zeyveli@gmail.com>
2026-02-20 21:34:51 +0300

    feat: player now conserves velocity.y while traveling through a portal

--- Assets/Scripts/Demo/FPSController.cs
+++ Assets/Scripts/Demo/FPSController.cs
@@ -115,12 +115,18 @@ public class FPSController : PortalTraveller {
 
     public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
         transform.position = pos;
+
         Vector3 eulerRot = rot.eulerAngles;
         float delta = Mathf.DeltaAngle (smoothYaw, eulerRot.y);
         yaw += delta;
         smoothYaw += delta;
         transform.eulerAngles = Vector3.up * smoothYaw;
+
         velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (velocity));
+
+        // conserve velocity.y
+        verticalVelocity = velocity.y;
+
         Physics.SyncTransforms ();
     }
 
